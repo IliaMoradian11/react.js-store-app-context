aff0349388a0db6a78772a4bb8fb8ff5bd9a34b7
Ilia Moradian <iliamoradian11@gmail.com>
2026-04-27 20:21:52 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam minima
+      laborum nihil, error rem obcaecati possimus doloremque eveniet quae vero
+      deleniti, quam est a quibusdam nemo accusamus maxime quo iste.
+    </p>
+  );
+}
+
+export default App;
