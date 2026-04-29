4905ba090fece747582e42def8f6755bd4bd4424
Ilia Moradian <iliamoradian11@gmail.com>
2026-04-29 20:59:55 +0330

    Add product details page

---
+++ src/components/ProductDetails.jsx
@@ -0,0 +1,55 @@
+import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { BiLeftArrowAlt } from "react-icons/bi";
+import { SiOpenproject } from "react-icons/si";
+import { IoMdPricetag } from "react-icons/io";
+
+import { context } from "../contexts/SearchContext";
+
+import styles from "./ProductDetails.module.css";
+
+function ProductDetails({ id }) {
+  const { products } = useContext(context);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      setProduct(products.find((productI) => productI.id === +id));
+    })();
+  });
+
+  return (
+    <div className={styles.container}>
+      <img src={product.image} alt={product.title} />
+      <div>
+        <h3>{product.title}</h3>
+        <p className={styles.text}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
+          iste dolores explicabo consectetur, rem eius ut quibusdam earum, sunt,
+          beatae enim ipsa! Quisquam suscipit nisi sequi similique molestias
+          autem inventore quod dolor quidem aliquid asperiores ipsam
+          perspiciatis animi possimus obcaecati beatae, delectus quam numquam
+          quas? Aliquid temporibus repellendus rerum neque doloribus laudantium,
+          nostrum consectetur modi similique inventore itaque, voluptatum
+          voluptates laborum saepe, provident sapiente? Pariatur, tempora.
+          Eveniet commodi iure eligendi sapiente amet voluptatem, quasi animi.
+        </p>
+        <div>
+          <p className={styles.categoryAndPrice}>
+            <SiOpenproject size={20} color="#fe5d42" />
+            <span>{product.category}</span>
+          </p>
+          <p className={styles.categoryAndPrice}>
+            <IoMdPricetag size={20} color="#fe5d42" />
+            <span>{product.price} $</span>
+          </p>
+        </div>
+        <Link to="/products">
+          <BiLeftArrowAlt /> <span>Back to Shop</span>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default ProductDetails;
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,5 +1,18 @@
+import { useParams } from "react-router-dom";
+
+import Layout from "../layouts/Layout";
+import ProductDetails from "../components/ProductDetails";
+
 function ProductDetailsPage() {
-  return <div>ProductDetailsPage</div>;
+  const { id } = useParams();
+
+  console.log(id);
+
+  return (
+    <Layout>
+      <ProductDetails id={id} />
+    </Layout>
+  );
 }
 
 export default ProductDetailsPage;
