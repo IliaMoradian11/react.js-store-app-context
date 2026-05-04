c468a3d3c5734f5ad5275ce8ae9297ed13964e44
Ilia Moradian <iliamoradian11@gmail.com>
2026-05-04 13:08:06 +0330

    change config.js and add error handler for interceptor

--- src/services/config.js
+++ src/services/config.js
@@ -4,14 +4,17 @@ const api = axios.create({ baseURL: "/src/assets" });
 
 api.interceptors.request.use(
   (request) => request,
-  (request) => console.log(request),
+  (request) => {
+    console.log(request);
+    return Promise.reject(request);
+  },
 );
 
 api.interceptors.response.use(
   (response) => response.data,
   (response) => {
     console.log(response);
-    Promise.reject(response);
+    return Promise.reject(response);
   },
 );
 
